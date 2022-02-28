05f7821d35144e469b198109fe55d79de36f8de6
ggarber42 <ggarber@protonmail.com>
2022-02-28 18:15:14 -0300

    metodo nativo do elemento que muda aparencia pra android e ios

--- lib/main.dart
+++ lib/main.dart
@@ -142,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('Show chart'),
-                  Switch(
+                  Switch.adaptive(
                       value: _showChart,
                       onChanged: (value) {
                         setState(() {
